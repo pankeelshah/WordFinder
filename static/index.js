fb9698ad9dfeb52475dfe80ded34be53d0e6bc2e
Pankeel Shah <shahx317@umn.edu>
2019-11-10 19:18:35 -0600

    modal text done

--- static/index.js
+++ static/index.js
@@ -9,18 +9,8 @@ async function getDefinition(key, word){
     jr.then( 
         function(data){
             console.log(data);
-            var dataLength = data.length;
-
-            for(var i = 0; i < dataLength; i++){
-                var ob = data[i];
-                //usage of word
-                var fl = data[i].fl;
-                //list of definitions of that type
-                var definition = data[i].shortdef;
-            }
-
-            openModal();            
-
+            var text = formatModalText(data);
+            openModal(text); 
         }
     ).catch(
         function(data){
@@ -29,8 +19,32 @@ async function getDefinition(key, word){
     )
 }
 
-function openModal(){
+function formatModalText(data){
+    text = "";
+    var dataLength = data.length;
+            
+    for(var i = 0; i < dataLength; i++){
+        var ob = data[i];
+        //usage of word
+        var fl = data[i].fl;
+        //list of definitions of that type
+        var definitions = data[i].shortdef;
+        
+        text += fl + "<br>";
+        var numberOfDefinitions = definitions.length;
+        for(var j = 0; j < numberOfDefinitions; j++){
+            text += (j+1).toString() + ". " + definitions[j] + "<br>";
+        }
+        text += "<br>";
+    }
+    return text;
+}
+
+function openModal(text){
     var modal = document.getElementById("myModal");
+    var modalText = document.getElementById("modalText");
+    
+    modalText.innerHTML = text;
     modal.style.display = "block";
 }
 
