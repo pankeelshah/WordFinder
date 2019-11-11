17886414847e4e173fd5f599e2853f8a3318409e
Pankeel Shah <shahx317@umn.edu>
2019-11-11 12:00:15 -0600

    removed test files

--- app.py
+++ app.py
@@ -54,3 +54,17 @@ def proxy():
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
+# import re
+
+# def findAllWords(fname):
+#     fp = open(fname)
+#     strings = re.findall(r'',fp.read())
+#     print(strings)
+
+# #findAllWords("test.txt")
+
+# n = ['yyyyy', 'aaaaa', 'yyy', 'aaa']
+# n.sort() # sorts normally by alphabetical order
+# print(n)
+# n.sort(key=len, reverse=False) # sorts by descending length
+# print(n)
